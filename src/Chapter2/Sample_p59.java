f68163c566a81de5f8ced34b35971fe024d53b29
matsubara <sayaka,matsubara@rakus.co.jp>
2014-11-17 17:17:14 +0900

    test commit

--- src/Chapter2/Sample_p59.java
+++ src/Chapter2/Sample_p59.java
@@ -3,9 +3,9 @@ package Chapter2;
 public class Sample_p59 {
 	public static void main(String[] args){
 
-		short s1 = 10;
-		s1 = ++s1;
-		s1 = s1 + 1;
+//		short s1 = 10;
+//		s1 = ++s1;
+//		s1 = s1 + 1;
 	}
 
 }
